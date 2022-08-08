9a0e28f31fb8267f7fbebd4f30d26f6c01eb7f09
Vincent Tjia <vtjia51@gmail.com>
2022-08-08 17:51:12 -0400

    added tyeDefs

---
+++ server/schemas/typeDefs.js
@@ -0,0 +1,38 @@
+const { gql } = require('apollo-server-express');
+
+const typeDefs = gpl`
+    type User {
+        _id: ID
+        username: String
+        email: String
+        bookCount: Interger
+        savedBooks: [Book]
+    }
+
+    type Book {
+        bookId: ID
+        authors: [String]
+        description: String
+        title: String
+        image: String
+        link: String
+    }
+
+    type Auth {
+        token: ID!
+        user: User
+    }
+
+    type Query {
+        me: User
+    }
+
+    type Mutation {
+        addUser(username: String!, email: String!, password: String!): Auth
+        login(email: String!, password: String!): Auth
+        saveBook(authors: [String], bookId: String, description: String!, image: String, link: String, title: String!): User
+        removeBook(bookId: String!): User
+    }
+`;
+
+module.exports = typeDefs;
