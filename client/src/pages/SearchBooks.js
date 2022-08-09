8488bb76eb18ca0654453efba966319275332064
Vincent Tjia <vtjia51@gmail.com>
2022-08-09 02:26:26 -0400

    added mutation function saveBook() to execute SAVE_BOOK mute by the apollo server

--- client/src/pages/SearchBooks.js
+++ client/src/pages/SearchBooks.js
@@ -4,6 +4,10 @@ import { Jumbotron, Container, Col, Form, Button, Card, CardColumns } from 'reac
 import Auth from '../utils/auth';
 import { saveBook, searchGoogleBooks } from '../utils/API';
 import { saveBookIds, getSavedBookIds } from '../utils/localStorage';
+import { useMutation } from '@apollo/client';
+import {useQuery} from '@apollo/client';
+import { SAVE_BOOK } from '../utils/mutations';
+import { QUERY_ME } from '../utils/queries';
 
 const SearchBooks = () => {
   // create state for holding returned google api data
@@ -65,7 +69,15 @@ const SearchBooks = () => {
     }
 
     try {
-      const response = await saveBook(bookToSave, token);
+      const response = await saveBook({
+        variables: {
+          bookId: bookToSave.bookId,
+          title: bookToSave.title,
+          authors: bookToSave.authors,
+          description: bookToSave.description,
+          image: bookToSave.image,
+        },
+      });
 
       if (!response.ok) {
         throw new Error('something went wrong!');
